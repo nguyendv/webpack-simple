9c54c0a0fdb0e87676908e1aea04d884508b531c
Dung Viet Nguyen <dvnguyen.vn@gmail.com>
2018-01-16 11:02:55 -0600

    Try to use express with npm run dev

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import express from 'express'
+import VueSSR from 'vue-server-renderer'
 
-new Vue({
+server = express()
+renderer = VueSSR.createRenderer()
+
+let vm = new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+server.get('*', (req, res) => {
+  renderer.renderToString(vm, (err, html) => {
+    if (er) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
+})
+
+server.listen(8080)
